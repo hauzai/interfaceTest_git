573b356b37fcc32f41bac001308f87715c154b10
yaoyihua <yaoyihua@togest.com>
2020-05-07 17:49:50 +0800

    1.修改GetResources类,将搜索条件放置在param中，url固定。

--- myrequest/getresources.py
+++ myrequest/getresources.py
@@ -1,18 +1,29 @@
 from myrequest import myrequests
+from myrequest.myerror import ResponseError
 
 
 class GetResources(myrequests.myrequests):
 
-    def __init__(self, url, method="GET", **kwargs):
-        super().__init__(method=method, url=url, **kwargs)
+    def __init__(self, resourcesCode, **kwargs):
+        self.params = {"allocationTag": "0", "dictName": "data_group", "resourcesCode": resourcesCode}
+        self.resourcesCode = resourcesCode
+        self.url = "http://192.168.1.96:8092/code/systemPropertyResources/resourcesCode"
+        self.kwargs = kwargs
+        self.kwargs["params"] = self.params
+        # data为查询到data数组
         self.data = None
-        self.response = super().run()
+        # response为调用run（）之后返回的response
+        self.response = None
+
+        super().__init__(url=self.url, **self.kwargs)
+
+
 
     def getresponse(self):
         return self.response.json()
 
     def getcustomName(self):
-        self.data = self.response.json()["data"]
+        # self.data = self.response.json()["data"]
         for i in self.data:
             print(i["id"]+" : "+i["customName"]+" : "+i["resource"])
 
@@ -28,11 +39,23 @@ class GetResources(myrequests.myrequests):
         else:
             print("输入错误")
 
+    def run(self):
+        super().run()
+        if self.response:
+            self.data = self.response.json()
+
+
+
+
+
 if __name__ == "__main__":
-    test = GetResources(url="http://192.168.1.96:8092/code/systemPropertyResources/resourcesCode?resourcesCode=issuedPlanDN&allocationTag=0&dictName=data_group", headers={"token": "2100000000000000"})
+    test = GetResources(resourcesCode="issuedPlanDN", headers={"token": "2100000000000000"})
     #print(test.getresponse())
     #print(test.getcustomName())
     #test.search_sourceItem(test="配属单位")
-    print(test.run())
-    test.getcustomName()
-    test.search_sourceItem(customName="公里标")
+    #print(test.run())
+    #test.getcustomName()
+    #test.search_sourceItem(customName="公里标")
+    print(test)
+    test.run()
+    print(test.data)
