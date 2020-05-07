e794b695bf87392edc6b26fca1169497083a9ffa
yaoyihua <yaoyihua@togest.com>
2020-05-07 08:43:45 +0800

    Initial commit

---
+++ myrequest/myrequests.py
@@ -0,0 +1,47 @@
+import requests
+
+class myrequests:
+
+    def __init__(self, method="GET", url="", **kwargs):
+        self.url = url
+        self.kwargs = kwargs
+        self.method = method
+
+    def __str__(self):
+        return "url:"+self.url+"\nmethod:"+self.method+"\nkwargs:"+str(self.kwargs)
+
+    def set_url(self, url):
+        self.url = url
+
+    def set_method(self, method):
+        self.method = str(method).upper()
+
+    def set_headers(self, headers):
+        if "headers" in self.kwargs.keys():
+            #self.kwargs["headers"] = headers
+            self.add_headers(headers)
+        else:
+            self.kwargs["headers"] = headers
+
+    def add_headers(self, dict_headers):
+        if "headers" in self.kwargs.keys():
+            for name in dict_headers.keys():
+                if name in self.kwargs["headers"]:
+                    print("header中值重复")
+                    continue
+                else:
+                    self.kwargs["headers"][name] = dict_headers[name]
+        else:
+            self.set_headers(dict_headers)
+
+    def run(self):
+        response = requests.request(self.method, self.url, **self.kwargs)
+        return response
+
+
+
+if __name__ == "__main__":
+    test = myrequests(url="http://192.168.1.96:8092/comp/subject/lists?authCode=LOOK_SUBJECT_MANAGEMENT&parentId=0", headers = {"token": "2100000000000000"})
+    test.set_headers({"token": "1"})
+    print(test)
+    print(test.run().text)
---
+++ util/myutil.py
@@ -0,0 +1,6 @@
+import urllib.parse as pa
+
+a = pa.urlparse("http://192.168.1.96:8092/jcw/produce/issuedPlan/issuedPlan/pages?orderBy=task_name+asc&pageSize=50&pageNo=1&authCode=LOOK_JCW_PLANMANAGE_TCDWRJH&planType=3&lineDegree=1" ,scheme="http")
+print(a.query)
+b = pa.parse_qs(a.query)
+print(b["orderBy1"])
