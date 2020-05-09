9bc07f61e7e1465e45e541c207ce5d00716336e6
hauzai <1017946794@qq.com>
2020-05-09 22:00:36 +0800

    config模块提交

--- util/config.py
+++ util/config.py
@@ -1,10 +1,17 @@
 import configparser
-# 生成config文件解析器
-cf = configparser.ConfigParser()
-# 读取config文件，此处使用绝对path，也可使用相对path
-cf.read(r"D:\interfaceTest_git\config.ini")
-# 展示配置文件中的所有section
-print(cf.sections())
-# get方法使用 sectionname，key 获取 value
-print(cf.get('test', 'path'))
+#  生成config文件解析器
+# cf = configparser.ConfigParser()
+#  读取config文件，此处使用相对路径
+# cf.read(r"..\config.ini")
+#  展示配置文件中的所有section
+# print(cf.sections())
+#  get方法使用 sectionname，key 获取 value
+# print(cf.get('96', 'ip'))
 
+def get_url(section):
+    cf = configparser.ConfigParser()
+    cf.read(r"..\config.ini")
+    return r"http://"+cf.get(section,"ip")+":"+cf.get(section,"port")
+
+if __name__ == '__main__':
+    print(get_url('96'))
